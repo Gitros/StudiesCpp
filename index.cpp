3f8285c2e3b669f3e4a0b7f2f629c491d8bf3295
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-27 17:04:11 +0200

    clear

--- index.cpp
+++ index.cpp
@@ -6,26 +6,4 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
-
-    cout << "Program sprawdza czy boki a, b i c tworzą trójkąt prostokątny." << endl;
-    cout << "Podaj bok a." << endl;
-    cin >> a;
-    cout << "Podaj bok b." << endl;
-    cin >> b;
-    cout << "Podaj bok c." << endl;
-    cin >> c;
-
-    if ((a * a + b * b) == (c * c))
-    {
-        cout << "Boki a = " << a << ", b = " << b << " i c = " << c;
-        cout << " tworzą trójkąt prostokątny." << endl;
-    }
-    else
-    {
-        cout << "Boki a = " << a << ", b = " << b << " i c = " << c;
-        cout << " nie tworzą trójkąta prostokątnego." << endl;
-    }
-
-    return 0;
 }
