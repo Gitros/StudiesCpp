5dff1f8f191e18544097f0bab9667d900b1ff99d
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-27 16:26:15 +0200

    modulo

---
+++ index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int a = 37;
+    int b = 11;
+
+    int result = a % b;
+
+    cout << fixed << result << endl;
+}
