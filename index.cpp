c762b1c889d7053db347d20b4438e3a8024d31f5
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-27 15:05:14 +0200

    exercise

--- index.cpp
+++ index.cpp
@@ -1,24 +1,13 @@
 #include <iostream>
-#include <iomanip>
-#include <conio.h>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    float a, b, pole;
+    double pi = M_PI;
 
-    cout << "Program oblicza pole prostokąta." << endl;
-    cout << "Podaj bok a." << endl;
-    cin >> a;
+    cout << "The value of pi is " << pi << endl;
 
-    cout << "Podaj bok b." << endl;
-    cin >> b;
-
-    pole = a * b;
-
-    cout << fixed << setprecision(2); // ustalenie precyzji
-    cout << "Pole prostokąta o boku a = " << a << " i boku b = " << b;
-    cout << " wynosi " << pole << "." << endl;
     return 0;
 }
