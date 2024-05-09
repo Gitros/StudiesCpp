ef1e30bdd7c5aa68cd67a447e44bd5319f9ff53d
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-05-09 17:23:07 +0200

    clear

--- index.cpp
+++ index.cpp
@@ -7,23 +7,4 @@ using namespace std;
 int main()
 
 {
-    int random = rand() % 10;
-    int number;
-
-    cout << "Zgadywanie liczby";
-    cout << endl
-         << "Podaj liczbe od 0 do 9: ";
-
-    cin >> number;
-
-    if (random == number)
-    {
-        cout << endl
-             << "Zgadłeś!";
-    }
-    else
-    {
-        cout << endl
-             << "Nie zgadłeś";
-    }
 }
