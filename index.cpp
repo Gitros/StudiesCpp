298fc3936d9accf6307f0913650137ce876531d3
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-05-16 18:14:19 +0200

    Potęga
    Napisz funkcję potega(x, n) liczącą dowolną n-tą potęgę liczby x. Zastanów się nad typem funkcji
    oraz typem przekazywanych argumentów.
    1. Początkowo zdefiniuj funkcję dla potęg o wykładniku dodatnim całkowitym (UWAGA na zero !)
    2. Poszerz definicję funkcji o liczby całkowite ujemne

--- index.cpp
+++ index.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+double potega(double x, double n, bool &error)
+{
+    if (x == 0 && n == 0)
+    {
+        error = true;
+        return 0; 
+    }
+    if (x == 0 && n < 0)
+    {
+        error = true;
+        return 0; 
+    }
+    if (n == 0)
+    {
+        error = false;
+        return 1; 
+    }
+
+    error = false;
+    return pow(x, n);
+}
+
 int main()
 {
-    int w1, k1, w2, k2;
+    double x, n;
+    bool error;
 
-    cin >> w1 >> k1 >> w2 >> k2;
-    cout << (w1 * k1) + (w2 * k2) << endl;
+    cout << "Podaj podstawę (x): ";
+    cin >> x;
+    cout << "Podaj wykładnik (n): ";
+    cin >> n;
+
+    double wynik = potega(x, n, error);
+
+    if (error)
+    {
+        cout << "Podano nieprawidłowe dane." << endl;
+    }
+    else
+    {
+        cout << x << " do potęgi " << n << " wynosi " << wynik << endl;
+    }
+    return 0;
 }
